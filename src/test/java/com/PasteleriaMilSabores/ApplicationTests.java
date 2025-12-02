49ef03ff96a4bc885dbfa428a9cc3868bdf6b7d8
Sol <sol.labbe@duocuc.cl>
2025-12-01 22:38:05 -0300

    commit

--- src/test/java/com/PasteleriaMilSabores/ApplicationTests.java
+++ src/test/java/com/PasteleriaMilSabores/ApplicationTests.java
@@ -1,9 +1,11 @@
 package com.PasteleriaMilSabores;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class ApplicationTests {
 
 	@Test
